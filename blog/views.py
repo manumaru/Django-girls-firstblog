3b77e1440ba695cbc6d923d158a4b0c176514a67
prana <fraimwing@gmail.com>
2020-07-20 22:50:45 +0900

    Modifi&fixed templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
+    #並べ替え　ここでもするの？
+    #この 変数 posts を使って、クエリセットのデータにアクセスする。だからかな。毎回整えて取得みたいな
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #htmlテンプレートを組み合わせるrender関数で得た値をreturn
-    return render(request, 'blog/post_list.html',{})
+    #リクエストというのは、インターネットを介してユーザから受け取った全ての情報が詰まったもの
+    #{} の中に引数を記述する時は、名前と値をセットにしなくてはなりません
+    return render(request, 'blog/post_list.html',{'posts': posts})
